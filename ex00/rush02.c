a12f837b8f99118a2049e28d87b8ba1fd0c02658
Benjy Malca Bautista <bmalca-b@car10s5.42barcelona.com>
2025-08-17 16:51:56 +0200

    Se validan los casos para numeros menores que 0.

--- ex00/rush02.c
+++ ex00/rush02.c
@@ -6,25 +6,25 @@
 /*   By: bmalca-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 13:01:12 by bmalca-b          #+#    #+#             */
-/*   Updated: 2025/08/16 14:27:19 by bmalca-b         ###   ########.fr       */
+/*   Updated: 2025/08/17 16:48:36 by bmalca-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	choose_letter(int x, int y, int h, int w)
+void	choose_letter(int x, int y, int w, int h)
 {
 	char	c;
 
-	if (x == 1 && (y == 1 || y == w))
+	if (y == 1 && (x == 1 || x == w))
 	{
 		c = 'A';
 	}
-	else if (x == h && (y == 1 || y == w))
+	else if (y == h && (x == 1 || x == w))
 	{
 		c = 'C';
 	}
-	else if (x == 1 || x == h || y == 1 || y == w)
+	else if (y == 1 || y == h || x == 1 || x == w)
 	{
 		c = 'B';
 	}
@@ -35,22 +35,24 @@ void	choose_letter(int x, int y, int h, int w)
 	ft_putchar(c);
 }
 
-void	rush(int w, int h)
+void	rush(int x, int y)
 {
-	int	x;
-	int	y;
+	int	to_print_x;
+	int	to_print_y;
 
-	x = 1;
-	y = 1;
-	while (x <= h)
+	to_print_x = 1;
+	to_print_y = 1;
+	if (x <= 0 || y <= 0)
+		return ;
+	while (to_print_y <= y)
 	{
-		while (y <= w)
+		while (to_print_x <= x)
 		{
-			choose_letter(x, y, h, w);
-			y++;
+			choose_letter(to_print_x, to_print_y, x, y);
+			to_print_x++;
 		}
-		x++;
+		to_print_y++;
 		ft_putchar('\n');
-		y = 1;
+		to_print_x = 1;
 	}
 }
